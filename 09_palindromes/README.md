45c640b5802e40428bb808f0e9c7ede054bfccb9
Yana Chackhovskaya <a9506862137@yandex.ru>
2023-11-21 14:20:26 +0300

    done

--- 09_palindromes/README.md
+++ 09_palindromes/README.md
@@ -16,4 +16,4 @@ palindromes('racecar') // true
 palindromes('tacos') // false
 ```
 
-
+npm test palindromes.spec.js
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,36 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let arr = [];
+    let arr2 = [];
+    let str2 = str.toLowerCase();
+    str2 = str2.replace('!', '');
+    str2 = str2.replace('.', '');
+   
+   for (let i = 0; i < str2.length; i++){
+    arr.push(str2.charAt(i));
+   }
 
+   arr2 = arr.slice(0);
+   arr2 = arr2.reverse();
+
+   let str3 = arr.join('');
+   let str4 = arr2.join('');
+
+   str3 = str3.replace(/,/g, '');
+   str3 = str3.replace(/\s+/g, '');
+
+   str4 = str4.replace(/,/g, '');
+   str4 = str4.replace(/\s+/g, ''); 
+
+   
+    
+    if (str3 == str4){
+        return true;
+    } else {
+        return false;
+    }
+    
+    
 };
-
+palindromes('racecar');
 // Do not edit below this line
 module.exports = palindromes;
